af22b5a58627be034d26277eb5566ccd93b1bfab
Shivam Yadav <shivamyadav456732@gmail.com>
2026-09-12 23:02:55 +0530

    LC 3414 Max score of non-overlap intervals bottomup and memo sol with intution and notes

---
+++ potd_contest/LC_3414/Memo.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int n;
+    struct Node{
+        long long score = -1;
+        vector<int>idxs;
+    };
+    vector<int>nextIdx;
+    vector<vector<Node>>dp;
+    Node solve(vector<vector<int>>& intervals,int i,int k){
+        if(k==0 || i>=n) return Node();
+        if(dp[i][k].score!=-1) return dp[i][k];
+        int wt = intervals[i][2];
+        int idx = intervals[i][3];
+        int j = nextIdx[i];
+        Node skip = solve(intervals,i+1,k);
+        Node temp = solve(intervals,j,k-1);
+        Node take;
+        take.score = temp.score + wt; 
+        take.idxs = temp.idxs;
+        take.idxs.push_back(idx);
+        sort(begin(take.idxs),end(take.idxs));
+        Node result;
+        if(skip.score > take.score) result = skip;
+        else if(skip.score < take.score) result = take;
+        else {
+            result = (skip.idxs < take.idxs) ? skip : take;
+        }
+        return dp[i][k] = result;
+
+    }
+    vector<int> maximumWeight(vector<vector<int>>& intervals) {
+        n = intervals.size();
+        // store starting index of each interval
+        for(int i=0;i<n;i++) intervals[i].push_back(i);
+        // sort intervals for start point
+        sort(intervals.begin(),intervals.end());
+        nextIdx.resize(n);
+
+        for(int i=0;i<n;i++){
+            int end = intervals[i][1];
+            nextIdx[i] = findNext(intervals,end); //binary search
+        }
+        int k = 4;
+        dp.assign(n+1,vector<Node>(k+1));
+        return solve(intervals,0,k).idxs;
+    }
+    int findNext(vector<vector<int>>& intervals,int r){
+        int low = 0,high=n-1;
+        int res = n;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(intervals[mid][0] > r){
+                res = mid;
+                high = mid - 1;
+            }else low  = mid+1;
+        }
+        return res;
+    }
+};
